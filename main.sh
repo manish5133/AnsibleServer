85a77adbfd6d4a60b922cb7089d713dce3d125bf
manish5133 <121598552+manish5133@users.noreply.github.com>
2023-01-10 22:33:20 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -76,3 +76,4 @@ ansible-playbook nova-compute.yml
 ansible-playbook neutron-controller.yml
 ansible-playbook neutron-compute.yml
 ansible-playbook openstack-dashboard.yml
+ansible-playbook cinder-controller.yml
